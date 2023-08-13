ffe28bbf777ab4d7625cbe2961dac81d0b7c806f
Vincent881909 <vini.kohm@gmx.de>
2023-08-13 18:20:09 +0200

    Code Refactoring

--- main/main.py
+++ main/main.py
@@ -1,157 +1,149 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
 import logging
 import os
 import sys
-
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
-from PIL import Image,ImageDraw,ImageFont
 import requests
 import datetime
 import time
 import traceback
-from waveshare_epd import epd2in9b_V3
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
 import matplotlib.pyplot as plt
-from PIL import Image, ImageEnhance, ImageFilter
-import datetime
 
+# Directories
+PIC_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+LIB_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(LIB_DIR):
+    sys.path.append(LIB_DIR)
+
+from waveshare_epd import epd2in9b_V3
+
+# E-paper dimensions: 298x126
 
 # Setup Currecncy API
-API_KEY = "60d868a310906f1923f9b632"
+API_KEY = '8137072dc2801b3010638855a4faefbb'
 BASE_CURRENCY = "EUR"
-TIME_STAMP = 0
-
-currency_symbols = {
+TARGET_CURRENCY = "ZAR"
+TREND_IN_WEEKS = 4
+CURRENCY_SYMBOLS = {
     'ZAR': '\u0052',  # South African Rand - U+0052
     'CAD': '\u0024',  # Canadian Dollar - U+0024
     'EUR': '\u20AC',  # Euro - U+20AC
 }
 
+# Init Fonts
+VARELA_ROOUND_18 = ImageFont.truetype(os.path.join(PIC_DIR, 'Varela_Round.ttf'), 18) 
+IBM_24 = ImageFont.truetype(os.path.join(PIC_DIR, 'IBM.ttf'), 24)
+IBM_18 = ImageFont.truetype(os.path.join(PIC_DIR, 'IBM.ttf'), 18) 
 
-def get_conversion(target_currency):
-    global TIME_STAMP
-    url = f'https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{BASE_CURRENCY}/{target_currency}'
-    response = requests.get(url)
-    response.raise_for_status()  
-    data = response.json()
-    conversion_rate = data['conversion_rate']
-    conversion_rate = round(conversion_rate,2)
-    TIME_STAMP = data['time_last_update_unix']
-    return f"{conversion_rate} {currency_symbols[target_currency]}"
+def timestamp_to_date(timestamp):
+    date = datetime.datetime.fromtimestamp(timestamp)
+    return date.strftime('%a %d %B %Y')
 
-def date_of_conversion():
-    date_object = datetime.datetime.fromtimestamp(TIME_STAMP)
-    date = date_object.strftime('%a %d %B %Y')
-    return f"{date}"
+def get_exchange_rate(date):
+    date = date.strftime('%Y-%m-%d')
+    url = f'http://api.exchangeratesapi.io/v1/{date}?access_key={API_KEY}&base={BASE_CURRENCY}&symbols={TARGET_CURRENCY}'
+    request = requests.get(url)
+    request.raise_for_status()
+    received_data = request.json()
+    exchange_rate = received_data['rates'][TARGET_CURRENCY]
+    timestamp = received_data['timestamp']
+    date = timestamp_to_date(timestamp)
+    return round(exchange_rate,2), date
 
-logging.basicConfig(level=logging.DEBUG)
+def fetch_currency_trend(weeks_duration):
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(weeks=weeks_duration)
+    num_days = (end_date - start_date).days
+    dates = []
+    exchange_rates = []
 
+    for day in range(num_days):
+        current_date = start_date + datetime.timedelta(days=day)
+        exchange_rates.append(get_exchange_rate(current_date)[0])
+        dates.append(current_date)
+        print(f'Trend: Fetching rate for date: {current_date}\n')
 
+    return exchange_rates, dates
 
-API_KEY1 = '8137072dc2801b3010638855a4faefbb'
-base = 'EUR'
-symbols = 'ZAR'
+def create_plot(exchange_rates, dates):
+    fig, ax = plt.subplots(dpi=300)  # Set DPI at creation time
+    ax.plot(dates, exchange_rates, marker='o')
+    ax.axis('off')
+    plt.grid(True)
+    plt.savefig('currency_trend.png', dpi=300, bbox_inches='tight', pad_inches=0)
 
-end_date = datetime.date.today()
-start_date = end_date - datetime.timedelta(weeks=4)
-num_days = (end_date - start_date).days
-dates = []
-exchange_rates = []
+def conduct_image_processing():
+    img = Image.open('currency_trend.png').convert('L')  # Convert to grayscale
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(2)
+    img = img.convert('1', dither=Image.NONE)
+    img = img.resize((200, 65))
+    img.save('currency_trend.bmp')
 
-for day in range(num_days):
-    current_date = start_date + datetime.timedelta(days=day)
-    URL = f'http://api.exchangeratesapi.io/v1/{current_date}?access_key={API_KEY1}&base={base}&symbols={symbols}'
-
-    request = requests.get(URL)
-    request.raise_for_status() 
-    data = request.json()
-    dates.append(current_date)
-    exchange_rates.append(data['rates'][symbols])
-
-
-
-#Plotting the trend and saving it
-fig, ax = plt.subplots(dpi=300)  # Set DPI at creation time
-ax.plot(dates, exchange_rates, marker='o')
-ax.axis('off')
-plt.grid(False)
-
-plt.savefig('exchange_rate.png', dpi=300, bbox_inches='tight', pad_inches=0)
-img = Image.open('exchange_rate.png').convert('L')  # Convert to grayscale
-enhancer = ImageEnhance.Contrast(img)
-img = enhancer.enhance(2)
-img = img.convert('1', dither=Image.NONE)
-img = img.resize((200, 65))
-img.save('exchange_rate.bmp')
-
-def trend_value():
+def trend_value(weeks_duration):
     todays_date = datetime.date.today()
-    start_date = todays_date - datetime.timedelta(weeks=4)
-
-    URL = f'http://api.exchangeratesapi.io/v1/{start_date}?access_key={API_KEY1}&base={base}&symbols={symbols}'
-    request = requests.get(URL)
-    request.raise_for_status() 
-    data = request.json()
-    old_rate = data['rates'][symbols]
-
-    URL = f'http://api.exchangeratesapi.io/v1/{todays_date}?access_key={API_KEY1}&base={base}&symbols={symbols}'
-    request = requests.get(URL)
-    request.raise_for_status() 
-    data = request.json()
-    todays_rate = data['rates'][symbols]
-
-
+    start_date = todays_date - datetime.timedelta(weeks=weeks_duration)
+    print(f'Trend Value: Fetching rate for date: {todays_date} and {start_date}.\n')
+    old_rate = get_exchange_rate(start_date)[0]
+    todays_rate = get_exchange_rate(todays_date)[0]
     trend = ((todays_rate - old_rate) / old_rate) * 100
-
     return round(trend,2)
 
-VARELA_ROOUND_18 = ImageFont.truetype(os.path.join(picdir, 'Varela_Round.ttf'), 18) 
-IBM_24 = ImageFont.truetype(os.path.join(picdir, 'IBM.ttf'), 24)
-IBM_18 = ImageFont.truetype(os.path.join(picdir, 'IBM.ttf'), 18) 
+def seconds_until_midnight():
+    now = datetime.datetime.now()
+    midnight = datetime.datetime.combine(now.date() + datetime.timedelta(days=1), datetime.time())
+    return int((midnight - now).total_seconds())
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 while True:
 
     try:
         epd = epd2in9b_V3.EPD()
-        logging.info("init and Clear")
+        logging.info("Init and Clear")
         epd.init()
         epd.Clear()
         time.sleep(1)
         
-        black_image = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-        red_image = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red  
+        black_image = Image.new('1', (epd.height, epd.width), 255) 
+        red_image = Image.new('1', (epd.height, epd.width), 255)
         draw_black = ImageDraw.Draw(black_image)
         draw_red = ImageDraw.Draw(red_image)
 
         #Left Hand Side View
         draw_black.rectangle((0, 0,75, 126), fill = 0)
-        draw_black.text((15, 23), 'EUR', font = IBM_24, fill = 1)
-        draw_black.text((15, 72), 'ZAR', font = IBM_24, fill = 1)
+        draw_black.text((15, 23), BASE_CURRENCY, font = IBM_24, fill = 1)
+        draw_black.text((15, 72), TARGET_CURRENCY, font = IBM_24, fill = 1)
 
         #Conversion Rate
-        if trend_value() >= 0:
-            draw_black.text((205, 106), f'+{trend_value()} %', font = VARELA_ROOUND_18, fill = 0)
-            draw_black.text((105, 104), get_conversion('ZAR'), font = IBM_18, fill = 0)
+        rate_trend_percentage = trend_value(TREND_IN_WEEKS)
+        current_exchange_rate = get_exchange_rate(datetime.datetime.now())
+      
+        if rate_trend_percentage >= 0:
+            draw_black.text((205, 106), f'+{rate_trend_percentage} %', font = VARELA_ROOUND_18, fill = 0)
+            draw_black.text((105, 104), f'{str(current_exchange_rate[0])} {CURRENCY_SYMBOLS[TARGET_CURRENCY]}', font = IBM_18, fill = 0)
         else:
-            draw_black.text((205, 106), f'{trend_value()} %', font = VARELA_ROOUND_18, fill = 0)
-            draw_red.text((105, 104), get_conversion('ZAR'), font = IBM_18, fill = 0)
+            draw_black.text((205, 106), f'{rate_trend_percentage} %', font = VARELA_ROOUND_18, fill = 0)
+            draw_red.text((105, 104), f'{str(current_exchange_rate[0])} {CURRENCY_SYMBOLS[TARGET_CURRENCY]}', font = IBM_18, fill = 0)
 
         #Conversion Time
-        draw_black.text((105, 10), date_of_conversion(), font = IBM_18, fill = 0)
+        date_of_conversion = current_exchange_rate[1]
+        draw_black.text((105, 10), date_of_conversion, font = IBM_18, fill = 0)
 
         #Currency Trend
-        newimage = Image.open('exchange_rate.bmp')
-        black_image.paste(newimage, (85,36))
+        currency_trend = fetch_currency_trend(4)
+        values = currency_trend[0]
+        dates = currency_trend[1]
+        create_plot(values,dates)
+        conduct_image_processing()
+        trend_img = Image.open('currency_trend.bmp')
+        black_image.paste(trend_img, (85,36))
 
         epd.display(epd.getbuffer(black_image), epd.getbuffer(red_image)) 
-  
-        #time.sleep(86400) # Upate every 24h 
-        #time.sleep(43200) # Upate every 12h 
-        time.sleep(21600) # Upate every 6h 
+        time.sleep(seconds_until_midnight())
             
     except IOError as e:
         logging.info(e)
