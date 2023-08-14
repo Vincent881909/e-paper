69d13b30fd8524c38870c826c5ffc38a67beb514
Vincent881909 <vini.kohm@gmx.de>
2023-08-14 17:21:09 +0200

    Added config file

--- main/main.py
+++ main/main.py
@@ -4,6 +4,7 @@ import os
 import sys
 import datetime
 import time
+import configparser
 
 # Directories
 LIB_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
@@ -15,6 +16,12 @@ import currency_api
 import draw
 from logger import logger
 
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.dirname(curr_dir)
+config_file_path = os.path.join(root_dir, "main", "config.cfg")
+config = configparser.ConfigParser()
+config.read(config_file_path)
+
 
 def main_script():
     try:
@@ -24,19 +31,19 @@ def main_script():
         epd.Clear()
 
         current_rate, conversion_date = currency_api.get_exchange_rate(datetime.datetime.now())
-        currency_trend = currency_api.fetch_currency_trend(currency_api.TREND_IN_WEEKS)
-        trend_percentage_change = currency_api.trend_value(currency_api.TREND_IN_WEEKS)
+        currency_trend = currency_api.fetch_currency_trend(int(config['DEFAULT']['TREND_IN_WEEKS']))
+        trend_percentage_change = currency_api.trend_value(int(config['DEFAULT']['TREND_IN_WEEKS']))
         logger.info("Data has been fetched")
 
         draw_black,draw_red,black_image,red_image = draw.init_canvas(epd.height,epd.width)
 
-        draw.currency_labels(draw_black,currency_api.BASE_CURRENCY,currency_api.TARGET_CURRENCY)
+        draw.currency_labels(draw_black,config["DEFAULT"]["BASE_CURRENCY"],config["DEFAULT"]["TARGET_CURRENCY"])
         draw.change_in_rate(trend_percentage_change,draw_black)
-        draw.exchange_rate(draw_black,draw_red,trend_percentage_change,current_rate,currency_api.CURRENCY_SYMBOLS[currency_api.TARGET_CURRENCY])
+        draw.exchange_rate(draw_black,draw_red,trend_percentage_change,current_rate,config["CURRENCY_SYMBOLS"][config["DEFAULT"]["TARGET_CURRENCY"]])
         draw.date_of_conversion(draw_black,conversion_date)
         draw.trend_graph(currency_trend,black_image)
         epd.display(epd.getbuffer(black_image), epd.getbuffer(red_image)) 
-        
+
         logger.info("Data has been displayed")
         logger.info("Script re-executed at midnight or until refreshed via web-gui")
             
